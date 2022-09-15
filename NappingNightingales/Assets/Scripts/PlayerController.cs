2a68db017dd19bb063f33e9f744e22657a552e76
mmaimer33 <rchousalkar@gmail.com>
2022-09-15 20:37:55 +0800

    Imported new input system and added basic player movement.

---
+++ NappingNightingales/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Fields
+
+    private Rigidbody2D rb2D;
+    private SpriteRenderer mySR;
+
+    // Movement support
+    private Vector2 movementInput;
+    [SerializeField]
+    private ContactFilter2D movementFilter;
+    private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    // TODO: change to const
+    [SerializeField]
+    private float movementSpeed = 3f;
+    [SerializeField]
+    private float collisionOffset = 0.05f;
+
+    #endregion
+
+    #region Methods
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        mySR = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Start()
+    {
+        castCollisions = new List<RaycastHit2D>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+    void FixedUpdate()  
+    {   
+        if (movementInput != Vector2.zero)
+        
+        {
+        
+            int numCollisions = rb2D.Cast(
+                movementInput,
+                movementFilter,
+                castCollisions,
+                movementSpeed * Time.fixedDeltaTime + collisionOffset
+                );
+
+            if (numCollisions == 0)
+            {
+                rb2D.MovePosition(rb2D.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+            }
+
+            //check direction
+            if (movementInput.x > 0) {
+                mySR.flipX = false;
+                //transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            } else if (movementInput.x < 0) {
+                mySR.flipX = true;
+                //transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+            }
+
+        }
+    }
+
+    #endregion
+}
