125cebd06142ad07af0bdfc021a2e0d1fc1e84d1
zrei <88964793+zrei@users.noreply.github.com>
2022-10-07 13:06:59 +0800

    Despawn bullets upon colliding with wall
    Also sets up skeleton code for bullets colliding with enemies.
    Shifts the spawn bullet code into an event manager. Bullet collisions
    with walls are also handled by the event manager

--- NappingNightingales/Assets/Scripts/Gameplay/BulletController.cs
+++ NappingNightingales/Assets/Scripts/Gameplay/BulletController.cs
@@ -6,34 +6,54 @@ public class BulletController : MonoBehaviour
 {
     #region Fields
 
-    private Rigidbody2D rb2D;
-    private SpriteRenderer mySR;
-    private int initialDir;
     private Transform transform;
-
+    
     [SerializeField]
     private float bulletSpeed = 5.5f;
 
+    private static int currId = 0;
+    private int id;
     #endregion
 
     #region Methods
 
-    void Awake() 
+    private void Awake() 
     {
-        rb2D = GetComponent<Rigidbody2D>();
-        mySR = GetComponent<SpriteRenderer>();
         transform = GetComponent<Transform>();
+        id = BulletController.currId;
+        BulletController.currId += 1;
     }
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        EventManager.current.onCollideWall += CollideWall;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.position += transform.up * Time.deltaTime * bulletSpeed;
+    }
+
+    //can be overridden
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        if (otherObject.gameObject.CompareTag("Wall"))
+        {
+            EventManager.current.CollideWall(this.id);
+        } else if (otherObject.gameObject.CompareTag("Enemy"))
+        {
+            //call some method from the enemy controller
+        }
+    }
+
+    //can be overridden
+    private void CollideWall(int bulletId) 
+    {
+        if (this.id == bulletId) {
+            Destroy(this.gameObject);
+        }
     }
 
     #endregion
---
+++ NappingNightingales/Assets/Scripts/Gameplay/EventManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager current;
+    public GameObject playerBullet;
+
+    void Awake() 
+    {
+        current = this;
+    }
+
+    void Start()
+    {
+
+    }
+        
+    public void SpawnBullet(Vector3 position, Quaternion rotation) { //can overload this later
+        Instantiate(playerBullet, position, rotation);
+    }
+
+    public event Action<int> onCollideWall; //needs to be set in the class
+    //if you want different behaviours of the coliision for different bullets you can have different classes
+    //inherit from the bullet controller and override the collision method
+    public void CollideWall(int id) 
+    {
+        if (onCollideWall != null) {
+            onCollideWall(id);
+        }
+    }
+
+}
