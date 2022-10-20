2e23d773e2eea42059ec6e0d308835e9d2a59801
alvinlim277 <alvinlim9815@gmail.com>
2022-10-20 20:10:04 +0800

    test game scene + pause

--- NappingNightingales/Assets/Scripts/Menus/PauseMenu.cs
+++ NappingNightingales/Assets/Scripts/Menus/PauseMenu.cs
@@ -8,6 +8,11 @@ public class PauseMenu : MonoBehaviour
 
     public GameObject pauseMenuUI;
 
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
